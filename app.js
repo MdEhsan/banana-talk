f6fdedb0fe482ab061e6f74425d3161dc6f19e8f
MdEhsan <mdehsan57@gmail.com>
2021-09-21 23:16:36 +0530

    style and js code added

---
+++ app.js
@@ -0,0 +1,34 @@
+const textInput = document.querySelector("#text-input")
+const outPut = document.querySelector("#text-output")
+const btnTranslate = documnet.querySelector(".translate-btn")
+
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+
+function getTranslateURL(input){
+    return serverURL + " ? " + "text= " + input;
+}
+
+
+function errorHandler(error){
+    console.log("error occoured" , error);
+    alert("Oops! Something went wrong. Try again later.")
+}
+
+
+function clickHandler(){
+
+    var inputText = textInput.value;
+
+    fetch(getTranslateURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+
+            outputDiv.innerText = translatedText;
+        }).catch(errorHandler)
+
+}
+
+btnTranslate.addEventListener("click", clickHandler)
